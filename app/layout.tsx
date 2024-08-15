ecaae5d7b4784ecc905fa95dd524b485dd606c4d
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-08-15 21:12:08 +0600

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      {/* <head>
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
-      </head>
+      </head> */}
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,11 +15,8 @@ const RecentProjects = () => {
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
-          <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
-          >
-            <Link href={item.link} target="_blank">
+          <Link key={item.id} href={item.link} target="_blank">
+            <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
               <PinContainer title={item.link} href={item.link}>
                 <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                   <div
@@ -72,8 +69,8 @@ const RecentProjects = () => {
                   </div>
                 </div>
               </PinContainer>
-            </Link>
-          </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
